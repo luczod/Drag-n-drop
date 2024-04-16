acfc7514b9d2904c9bd42d12816771d2f94d8965
luczod <kzod5611@gmail.com>
2024-04-16 19:24:13 -0300

    feat: change style from parent node

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -72,9 +72,9 @@ export function Card({
     <li
       ref={liRef}
       aria-pressed={isDragging}
-      className="flex items-center py-2 px-3 aria-pressed:bg-transparent aria-pressed:hover:scale-100 aria-pressed:border-2 aria-pressed:border-gray-400 aria-pressed:border-dashed cursor-grab hover:scale-110 mx-auto transition duration-300 ease-in-out border-t-[20px] border-t-[rgba(230,236,245,0.4)] bg-white shadow-md rounded-sm"
+      className="flex items-center py-2 px-3 [&>span]:aria-pressed:invisible aria-pressed:bg-transparent aria-pressed:hover:scale-100 aria-pressed:border-2 aria-pressed:border-gray-400 aria-pressed:border-dashed cursor-grab hover:scale-110 mx-auto transition duration-300 ease-in-out border-t-[20px] border-t-[rgba(230,236,245,0.4)] bg-white shadow-md rounded-sm"
     >
-      <span aria-pressed={isDragging} className="space-y-4 aria-pressed:invisible ">
+      <span className="space-y-4">
         <p className="text-base font-medium uppercase leading-none">{card.content}</p>
         <PersonIcon className="border-2 mt-2 size-6" />
       </span>
